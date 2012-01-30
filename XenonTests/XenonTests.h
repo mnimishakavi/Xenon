de56b5d6a9fb20bc7e3b40acb5dc5ce27909bdbb
Mahi <Sali@HSCINHYDLTMAC01.local>
2012-01-30 18:12:47 +0530

    Initial Commit

---
+++ XenonTests/XenonTests.h
@@ -0,0 +1,13 @@
+//
+//  XenonTests.h
+//  XenonTests
+//
+//  Created by SadikAli on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XenonTests : SenTestCase
+
+@end
---
+++ XenonTests/XenonTests.m
@@ -0,0 +1,32 @@
+//
+//  XenonTests.m
+//  XenonTests
+//
+//  Created by SadikAli on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XenonTests.h"
+
+@implementation XenonTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in XenonTests");
+}
+
+@end
